0686be5686ef7cbfbc0fb53faa271a63f4c07359
l-404-l <saywhattowhat@gmail.com>
2020-11-01 23:50:27 -0600

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: MelonInfo(typeof(emmVRCUnlocker.Main), "EmmVRC_Unlocker", "1.0", "github.com/l-404-l")]
+[assembly: MelonInfo(typeof(emmVRCUnlocker.Main), "emmVRCUnlocker", "1.0", "github.com/l-404-l")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace emmVRCUnlocker
@@ -17,10 +17,10 @@ namespace emmVRCUnlocker
     {
         internal static FieldInfo RiskF = AccessTools.Field(typeof(emmVRC.Managers.RiskyFunctionsManager), "riskyFuncsAllowed");
         internal static FieldInfo RiskCF = AccessTools.Field(typeof(emmVRC.Managers.RiskyFunctionsManager), "RiskyFunctionsChecked");
+        
         public override void OnApplicationStart()
         {
-            var value = Harmony.HarmonyInstance.Create("EmmVRC_FuckYourLimits" + Guid.NewGuid().ToString());
-            value.Patch(typeof(emmVRC.Managers.RiskyFunctionsManager).GetProperty("RiskyFunctionsAllowed").GetGetMethod(), new HarmonyMethod(typeof(Main), "CheckFixed"));
+            harmonyInstance.Patch(typeof(emmVRC.Managers.RiskyFunctionsManager).GetProperty("RiskyFunctionsAllowed").GetGetMethod(), new HarmonyMethod(typeof(Main), "CheckFixed")); // Yes its this simple. LOL
         }
         public static bool CheckFixed(bool __result)
         {
