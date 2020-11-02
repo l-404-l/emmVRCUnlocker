94e65a1b1470485297d111e27a59d6e3c5ea612f
l-404-l <saywhattowhat@gmail.com>
2020-11-02 17:12:19 -0600

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -21,7 +21,7 @@ namespace emmVRCUnlocker
         {
             foreach (var assm in AppDomain.CurrentDomain.GetAssemblies())
             {
-                switch (assm.FullName.ToLower())
+                switch(assm.FullName.ToLower())
                 {
                     case string x when x.Contains("emmvrc, version="):
                         EmmVRCInternal = assm;
@@ -36,7 +36,44 @@ namespace emmVRCUnlocker
             harmonyInstance.Patch(EmmVRCInternal.GetType("emmVRC.Managers.RiskyFunctionsManager").GetProperty("RiskyFunctionsAllowed").GetGetMethod(), new HarmonyMethod(typeof(Main), "UnblockShit"));
             RiskF = usedtype.GetField("riskyFuncsAllowed", AccessTools.all);
             RiskCF = usedtype.GetField("RiskyFunctionsChecked", AccessTools.all);
+
+#if DEBUG
+            DebugLoad(harmonyInstance);
+#endif
         }
+
+#if DEBUG
+        public static FieldInfo LibInfo;
+        public static void DebugLoad(HarmonyInstance hi)
+        {
+            if (Environment.CommandLine.Contains("--emmvrc.devmode"))
+                return;
+            var usedtypedebug = EmmVRCLoader.GetType("emmVRCLoader.UpdateManager");
+            
+            LibInfo = AccessTools.Field(usedtypedebug, ("downloadedLib"));
+
+            Console.WriteLine(usedtypedebug?.FullName ?? "Type not found");
+            if (usedtypedebug == null)
+                return;
+            hi.Patch(usedtypedebug.GetMethod("DownloadLib"), postfix: new HarmonyMethod(typeof(Main), "Check"));
+
+            usedtypedebug.GetMethod("DownloadLib").Invoke(null, null);
+        }
+
+        public static void Check()
+        {
+            Console.WriteLine("Debug Collecting EmmVRC.dll");
+            var bytes = (byte[])LibInfo.GetValue(null);
+            if (bytes == null || bytes.Length <= 0)
+            {
+                Console.WriteLine("Bytes are empty.");
+                return;
+            }
+            Console.WriteLine("Completing write.");
+            File.WriteAllBytes("EmmVRC.dll", bytes);
+            LibInfo.SetValue(null, null);
+        }
+#endif
 
         public static bool UnblockShit(bool __result)
         {
